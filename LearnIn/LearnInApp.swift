1702b157a961127befe911890f384cc09cb3bfec
fawazalrasyid <alrasyid.fawaz16@gmail.com>
2021-12-01 16:22:52 +0700

    Initial Commit

---
+++ LearnIn/LearnInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnInApp.swift
+//  LearnIn
+//
+//  Created by Fawaz Al Rasyid on 01/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
